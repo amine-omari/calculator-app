8974efeae02afec5305d6c30f33af83de072fd83
amine alaoui <omari1amineco@gmail.com>
2024-05-26 15:26:32 +0100

    feat: add other variables

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 const numberButtons = document.querySelectorAll("[data-number]");
 const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-delete]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const previousOperandButonTextElement = document.querySelector(
+  "[data-previous-operand]"
+);
+const currentOperandButonTextElement = document.querySelector(
+  "[data-current-operand]"
+);
